d66e2f8e495dc7ae2477a292159b56f952e26ee0
Marsboy02 <marsboy0619@gmail.com>
2024-06-10 19:34:58 +0900

    feat: prod 환경 배포를 위한 세팅 추가

--- main.py
+++ main.py
@@ -1,12 +1,21 @@
-from Flask import Flask
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+cors = CORS(app, resources={
+  r"/*": {"origin": "*"},
+})
 
 
 @app.route('/')
 def home():
-    return "hello world!"
+    return "Server is healthy"
+
+
+@app.route('/image', methods=['POST'])
+def ocr():
+    return "OCR"
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, port=5000)
